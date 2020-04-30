9f1aac01cfaeb2bd1f2a65ea98696c5fb6a05c36
Cascade Herriott <32555757+cascadeherriott@users.noreply.github.com>
2020-04-30 09:00:41 -0700

    Update RankedGradeBook.cs
    Section 4 test

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -31,5 +31,21 @@ namespace GradeBook.GradeBooks
             return 'F';
 
         }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count>4)
+                base.CalculateStatistics();
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count > 4)
+                base.CalculateStudentStatistics(name);
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+        }
     } 
 }
